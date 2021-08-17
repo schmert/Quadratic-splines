90f90054f4a62ac7f2662b4e85af3639cf206f96
Carl Schmertmann <schmertmann@fsu.edu>
2021-08-17 09:57:38 -0400

    small improvements -- added colored sections

--- QS-interactive/app.R
+++ QS-interactive/app.R
@@ -5,8 +5,10 @@ library(tidyverse)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Quadratic Spline Fertility Model"),
-
+    titlePanel("Quadratic Spline Fertility Model: Illustration"),
+    titlePanel(h6("This app illustrates the Quadratic Spline (QS) model proposed in Schmertmann (2003)")),
+    titlePanel(h6("Parameters \u03b1, P, H, and R control the shape and level.")),
+    titlePanel(h6("Colored sections illustrate the piecewise quadratic curves that comprise the spline schedule")),
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(width=2,
@@ -87,32 +89,40 @@ server <- function(input, output) {
                     x=as.vector(x) ,y=as.vector(y) ))
         
     } #QS
-    
+  
+  
     output$fx_plot <- renderPlot({
-        x    <- seq(10,55,.25)
-        fx   <- QS(x, input$R, input$alpha, input$P, input$H)$y
 
-        model = tibble(x,fx)
+        x       = seq(10,55,.25)
+        this_QS = QS(x, input$R, input$alpha, input$P, input$H)
+        fx      = this_QS$y
+      
+        dividers = c(-Inf,this_QS$knot,this_QS$beta,Inf)
+        model = tibble(x,fx, section=cut(x,breaks=dividers))
         
         G = ggplot(data=model) + 
-            aes(x=x,y=fx) +
-            geom_line(color='red', size=3, alpha=.80) +
+            aes(x=x,y=fx, color=section) +
+            geom_line(size=5, alpha=.60) +
+            geom_line(aes(x=x,y=fx),size=1.5, color='black') +
             labs(x='Age',y='Fertility Rate per 1000') +
             theme_bw() +
             theme(axis.text  = element_text(size=14,face='bold'),
                   axis.title = element_text(size=14,face='bold')) +
-            geom_point(x=input$P, y=0, shape=16, size=4) +
-            geom_text(x=input$P+1,y=10, label='P', size=6) +
-            geom_segment(x=input$P, y=0, xend=input$P, yend = input$R, size=1 ) +
-            geom_point(x=input$H, y=0, shape=16, size=4) +
-            geom_text(x=input$H+1,y=10, label='H', size=6) +
-            geom_segment(x=input$H, y=0, xend=input$H, yend = input$R/2, size=1 ) +
-            geom_point(x=input$alpha, y=0, shape=16, size=4) +
-            geom_hline(yintercept = c(0,input$R,input$R/2), lty='dotted', size=1) +
-            geom_text(x=10,y=0.95*input$R, label='R', size=6) +
-            geom_text(x=10,y=0.45*input$R, label='R/2', size=6) +
-            geom_text(x=input$alpha,y=0.05*input$R, label="\u03b1", size=6) +
-            scale_y_continuous(expand=c(0.02,0.02))
+            geom_point(x=input$P, y=0, shape=16, size=4,color='black') +
+            geom_text(x=input$P+1,y=10, label='P', size=6,color='black') +
+            geom_segment(x=input$P, y=0, xend=input$P, yend = input$R, size=1,color='black' ) +
+            geom_point(x=input$H, y=0, shape=16, size=4,color='black') +
+            geom_text(x=input$H+1,y=10, label='H', size=6,color='black') +
+            geom_segment(x=input$H, y=0, xend=input$H, yend = input$R/2, size=1,color='black' ) +
+            geom_point(x=input$alpha, y=0, shape=16, size=4,color='black') +
+            geom_hline(yintercept = c(0,input$R,input$R/2), 
+                       lty='dashed', size=0.8,color='black') +
+            geom_text(x=10,y=0.95*input$R, label='R', size=6,color='black') +
+            geom_text(x=10,y=0.45*input$R, label='R/2', size=6,color='black') +
+            geom_text(x=input$alpha,y=0.05*input$R, label="\u03b1", size=6,color='black') +
+            scale_y_continuous(expand=c(0.02,0.02)) +
+            scale_x_continuous(breaks=seq(10,55,5),minor_breaks = NULL) +
+            guides(color=FALSE)
             
         
         
